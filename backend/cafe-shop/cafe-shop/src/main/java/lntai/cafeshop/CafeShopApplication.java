2a9bdc562f15f75c63457f611016b997759dfb2a
taiz <llamttai@gmail.com>
2023-11-08 09:23:30 +0700

    add backend

---
+++ backend/cafe-shop/cafe-shop/src/main/java/lntai/cafeshop/CafeShopApplication.java
@@ -0,0 +1,13 @@
+package lntai.cafeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeShopApplication.class, args);
+	}
+
+}
---
+++ backend/cafe-shop/cafe-shop/src/test/java/lntai/cafeshop/CafeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package lntai.cafeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
