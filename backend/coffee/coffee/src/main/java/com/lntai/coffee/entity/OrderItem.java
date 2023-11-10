b390cca2de38b2dbc985e7216794360f1a63892e
taiz <llamttai@gmail.com>
2023-11-10 09:00:25 +0700

    commit

--- backend/coffee/coffee/src/main/java/com/lntai/coffee/entity/OrderItem.java
+++ backend/coffee/coffee/src/main/java/com/lntai/coffee/entity/OrderItem.java
@@ -1,2 +1,25 @@
-package com.lntai.coffee.entity;public class OrderItem {
+package com.lntai.coffee.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+
+@Entity
+@Table(name = "OrderItem")
+@Data
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_item_id")
+    private int orderItemId;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private int quantity;
 }
