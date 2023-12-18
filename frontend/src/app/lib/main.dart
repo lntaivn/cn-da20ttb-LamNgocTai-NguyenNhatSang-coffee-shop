5d54692c65f5857c4d24efb3440c484f02312135
nnsang1309 <nguyennhatsang2002@gmail.com>
2023-12-18 12:19:38 +0700

    update login

--- frontend/src/app/lib/main.dart
+++ frontend/src/app/lib/main.dart
@@ -1,103 +1,164 @@
+import 'package:app/screen/table_list.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LoginPage(),
+      home: Login(),
     );
   }
 }
 
-class LoginPage extends StatelessWidget {
+class Login extends StatefulWidget {
+  const Login({super.key});
+
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Đăng nhập'),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Tên đăng nhập',
-              ),
-            ),
-            SizedBox(height: 10.0),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Mật khẩu',
-              ),
-              obscureText: true,
-            ),
-            SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () {
-                // Xử lý đăng nhập ở đây
-              },
-              child: Text('Đăng nhập'),
-            ),
-            SizedBox(height: 10.0),
-            TextButton(
-              onPressed: () {
-                // Điều hướng đến trang đăng ký
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => RegisterPage()));
-              },
-              child: Text('Chưa có tài khoản? Đăng ký ngay!'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+  _LoginState createState() => _LoginState();
 }
 
-class RegisterPage extends StatelessWidget {
+class _LoginState extends State<Login> {
+  TextEditingController _usernameController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  void _login() {
+    // Thực hiện xác thực tại đây, ví dụ: kiểm tra tên người dùng và mật khẩu
+    String username = _usernameController.text;
+    String password = _passwordController.text;
+
+    // Giả sử xác thực thành công nếu username và password không rỗng
+    if (username.isNotEmpty && password.isNotEmpty) {
+      // Chuyển sang màn hình thứ hai khi đăng nhập thành công
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => TableOrder()),
+      );
+    } else {
+      // Hiển thị thông báo lỗi nếu đăng nhập không thành công
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Đăng nhập không thành công'),
+            content: Text('Vui lòng kiểm tra lại tên người dùng và mật khẩu.'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('Đóng'),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Đăng ký'),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Tên đăng nhập',
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
+          constraints: BoxConstraints.expand(),
+          color: Colors.white,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+                child: Container(
+                    width: 70,
+                    height: 70,
+                    padding: EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle, color: Color(0xffd8d8d8)),
+                    child: FlutterLogo()),
               ),
-            ),
-            SizedBox(height: 10.0),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Mật khẩu',
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 60),
+                child: Text(
+                  "Hello\nWelcome Back",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      fontSize: 30),
+                ),
               ),
-              obscureText: true,
-            ),
-            SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () {
-                // Xử lý đăng ký ở đây
-              },
-              child: Text('Đăng ký'),
-            ),
-            SizedBox(height: 10.0),
-            TextButton(
-              onPressed: () {
-                // Điều hướng quay lại trang đăng nhập
-                Navigator.pop(context);
-              },
-              child: Text('Quay lại đăng nhập'),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+                child: TextField(
+                  style: TextStyle(fontSize: 18, color: Colors.black),
+                  controller: _usernameController,
+                  decoration: InputDecoration(
+                      labelText: "USERNAME",
+                      labelStyle:
+                          TextStyle(color: Color(0xff88888888), fontSize: 15)),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+                child: Stack(
+                  alignment: AlignmentDirectional.centerEnd,
+                  children: <Widget>[
+                    TextField(
+                      style: TextStyle(fontSize: 18, color: Colors.black),
+                      obscureText: true,
+                      controller: _passwordController,
+                      decoration: InputDecoration(
+                          labelText: "PASSWORD",
+                          labelStyle: TextStyle(
+                              color: Color(0xff88888888), fontSize: 15)),
+                    ),
+                    Text(
+                      "SHOW",
+                      style: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 13,
+                          fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+                child: SizedBox(
+                    width: double.infinity,
+                    height: 56,
+                    child: ElevatedButton(
+                      onPressed: _login,
+                      child: Text(
+                        "SIGN IN",
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      ),
+                    )),
+              ),
+              Container(
+                height: 130,
+                width: double.infinity,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      "NEW USER? SIGN UP",
+                      style:
+                          TextStyle(fontSize: 15, color: Color(0xff88888888)),
+                    ),
+                    Text(
+                      "FORGOT PASSWORD?",
+                      style: TextStyle(fontSize: 15, color: Colors.lightBlue),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
